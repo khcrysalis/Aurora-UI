6f47f124ce0ceea6f2f14beccee9c2a64e7de32b
flower <97859147+flowerible@users.noreply.github.com>
2023-06-15 22:18:44 -0700

    Initial Commit

---
+++ rambutan/rambutanApp.swift
@@ -0,0 +1,17 @@
+//
+//  rambutanApp.swift
+//  rambutan
+//
+//  Created by samara on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct rambutanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

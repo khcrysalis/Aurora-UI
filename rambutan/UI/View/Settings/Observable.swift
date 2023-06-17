e8ce61d2a006c5970d35ef2f7715eba21dfce45d
Lakhan Lothiyi <llsc12.dev@gmail.com>
2023-06-17 23:47:02 +0100

    device info

--- rambutan/UI/View/Settings/Observable.swift
+++ rambutan/UI/View/Settings/Observable.swift
@@ -9,19 +9,31 @@ import SwiftUI
 import MachO
 
 class Settings: ObservableObject {
-    @Published var isRestoreRootfsEnabled = false
-    @Published var isTweaksEnabled = true
-    @Published var isOTAEnabled = true
-    @Published var isLogsEnabled = true
-    static let shared = Settings()
+  @Published var isRestoreRootfsEnabled = false
+  @Published var isTweaksEnabled = true
+  @Published var isOTAEnabled = true
+  @Published var isLogsEnabled = true
+  static let shared = Settings()
 }
 
 struct DeviceInfo {
-    static let version = UIDevice.current.systemVersion
-    static let arch = String(cString: NXGetLocalArchInfo().pointee.name)
-    let d = HostManager.self
-    let machinename = d.getModelName() ?? "Unknown"
-    let modelarch = d.getModelArchitecture() ?? "Unknown"
-    let platformname = d.getPlatformName() ?? "Unknown"
-    let platformver = d.getPlatformVersion() ?? "Unknown"
+  
+  static let current = DeviceInfo()
+  
+  let version = UIDevice.current.systemVersion
+  let arch = String(cString: NXGetLocalArchInfo().pointee.name)
+  
+  init() {
+	let d = HostManager.self
+	
+	self.machineName = d.getModelName() ?? "Unknown"
+	self.modelArch = d.getModelArchitecture() ?? "Unknown"
+	self.platformName = d.getPlatformName() ?? "Unknown"
+	self.platformVer = d.getPlatformVersion() ?? "Unknown"
+  }
+  
+  let machineName: String
+  let modelArch: String
+  let platformName: String
+  let platformVer: String
 }

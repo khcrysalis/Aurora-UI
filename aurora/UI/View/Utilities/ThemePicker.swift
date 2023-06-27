a4c9ad9805534b1aa03817461cf0af803e73830f
flower <97859147+flowerible@users.noreply.github.com>
2023-06-26 23:27:51 -0700

    feat: replacement scarlet wallpaper

--- aurora/UI/View/Utilities/ThemePicker.swift
+++ aurora/UI/View/Utilities/ThemePicker.swift
@@ -12,6 +12,7 @@ struct Theme: Identifiable, Hashable {
         Theme(name: "Aurora", image: "milad-fakurian-u8Jn2rzYIps-unsplash"),
         Theme(name: "Azure", image: "magicpattern-65O4Dw6-xLg-unsplash"),
         Theme(name: "Borealis", image: "northern-lights"),
+        Theme(name: "Crawford", image: "absolutely-flowerible"),
         Theme(name: "Scarlet", image: "northern-lights-red")
     ]
     
